02ab85e4a5475544d016bee1030491642776d568
Sas <ilansas4@gmail.com>
2019-09-25 23:33:31 +0200

    add couple of instrumented tests

--- app/src/androidTest/java/com/sas/cat_stuff/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sas/cat_stuff/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.sas.cat_stuff
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.doubleClick
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.sas.cat_stuff.home.activity.HomeActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,25 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule  = IntentsTestRule(HomeActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.sas.cat_stuff", appContext.packageName)
+    }
+
+    @Test
+    fun test() {
+        onView(withId(R.id.show_me_more_button)).perform(click())
+
+        onView(withId(R.id.first_cat)).perform(doubleClick())
+
+        onView(withId(R.id.likes)).perform(click())
+        intended(hasComponent("com.sas.cat_stuff.likes.activity.LikesActivity"))
     }
 }
--- data/src/main/java/com/sas/data/retrofit/CatApiCalls.kt
+++ data/src/main/java/com/sas/data/retrofit/CatApiCalls.kt
@@ -1,15 +1,15 @@
 package com.sas.data.retrofit
 
-import com.sas.data.model.*
+import com.sas.data.model.AddFavorite
+import com.sas.data.model.Favorite
+import com.sas.data.model.Image
+import com.sas.data.model.PostResult
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface CatApiCalls {
-
-    @GET("breeds")
-    suspend fun getBreeds(@Query("page") page: Int, @Query("limit") limit: Int = 10): List<Breed>
 
     @GET("images/search")
     suspend fun getImages(@Query("limit") limit: Int = 3): List<Image>
